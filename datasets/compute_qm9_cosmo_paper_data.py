fixed bug in computation script

datasets/compute_qm9_cosmo_paper_data.py
@@ -3,6 +3,10 @@ import ase.io
 
 frames = ase.io.read('qm9_30000.xyz', ':') + ase.io.read('qm9_5000.xyz', ':')
 property_values = np.hstack( (np.load('qm9_eV_30000.dHf_peratom.npy'), np.load('qm9_eV_5000.dHf_peratom.npy')) )
-for frame in frames:
-    frame.info['eV/atom'] = property_values
+
+for i in range(len(frames)):
+    frames[i].info['eV/atom'] = property_values[i]
+    #frames[i].cell = np.eye(3) * 100
+    #frames[i].center()
+    #frames[i].wrap(eps=1e-11)
 ase.io.write("qm9_cosmo_paper.extxyz", frames)